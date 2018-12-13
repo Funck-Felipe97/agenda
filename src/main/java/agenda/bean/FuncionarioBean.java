5f06c253559ae174421153dabdc6d9a288e94e4a
Felipe Carvalho Funck <felipe.funck@jlinformatica.net.br>
2018-12-13 00:05:12 -0200

    Meus commits

--- src/main/java/agenda/bean/FuncionarioBean.java
+++ src/main/java/agenda/bean/FuncionarioBean.java
@@ -1,7 +1,11 @@
 package agenda.bean;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import agenda.exceptions.ObjetoJaCadastradoException;
 import agenda.model.Funcionario;
@@ -10,17 +14,17 @@ import agenda.service.FuncionarioService;
 
 @ManagedBean
 @SessionScoped
-public class FuncionarioBean extends AbstractBean{
+public class FuncionarioBean extends AbstractBean {
 
 	private Funcionario funcionario;
 	private FuncionarioService funcionarioService;
 	private String confirmarSenha;
-		
+
 	public FuncionarioBean() {
 		this.funcionario = new Funcionario();
 		this.funcionarioService = new FuncionarioService();
 	}
-	
+
 	public void save() {
 		if (funcionario.getUsuario().getSenha().equals(confirmarSenha)) {
 			try {
@@ -30,38 +34,48 @@ public class FuncionarioBean extends AbstractBean{
 				this.funcionario = new Funcionario();
 			} catch (ObjetoJaCadastradoException e) {
 				super.warn("Usuario já existente");
-			} catch(Exception e) {
+			} catch (Exception e) {
 				super.error("Ocorreu uma falha");
 			}
 		} else {
 			super.error("As senhas são diferentes");
-			this.funcionario = new Funcionario();
 		}
-		
+
 	}
-	
+
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
-	
+
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
 	}
-	
+
 	public FuncionarioService getFuncionarioService() {
 		return funcionarioService;
 	}
-	
+
 	public void setFuncionarioService(FuncionarioService funcionarioService) {
 		this.funcionarioService = funcionarioService;
 	}
-	
+
 	public String getConfirmarSenha() {
 		return confirmarSenha;
 	}
-	
+
 	public void setConfirmarSenha(String confirmarSenha) {
 		this.confirmarSenha = confirmarSenha;
 	}
-	
+
+	public void logout() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+		session.invalidate();
+		try {
+			context.getExternalContext().redirect("../index.xhtml");
+		} catch (IOException ex) {
+
+		}
+	}
+
 }
--- src/main/java/agenda/bean/FuncionarioLogadoBean.java
+++ src/main/java/agenda/bean/FuncionarioLogadoBean.java
@@ -8,21 +8,44 @@ import javax.faces.bean.SessionScoped;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import agenda.dao.FuncionarioDao;
 import agenda.dao.UsuarioDao;
 import agenda.model.Funcionario;
 import agenda.model.Usuario;
 
 @SessionScoped
 @ManagedBean
-public class FuncionarioLogadoBean {
+public class FuncionarioLogadoBean extends AbstractBean{
 	
 	private Funcionario funcionario;
+	private String confirmarSenha;
 
 	public Funcionario getFuncionario() {
 		Usuario usuario = new Usuario();
 		usuario.setLogin(((SecurityContext) SecurityContextHolder.getContext()).getAuthentication().getName());
 		funcionario = new UsuarioDao().findByLogin(usuario.getLogin()).getFuncionario();
 		return funcionario;
+	}
+	
+	public void update() {
+		if (this.funcionario.getUsuario().getSenha().equals(confirmarSenha)) {
+			try {
+				new FuncionarioDao().update(this.funcionario);
+				super.info("Informações salvas com sucesso");
+			} catch (Exception e) {
+				super.error("Ocorreu uma falha ao salvar as informações");
+			}
+		} else {
+			super.info("As senha são diferentes");
+		}
+	}
+	
+	public String getConfirmarSenha() {
+		return confirmarSenha;
+	}
+	
+	public void setConfirmarSenha(String confirmarSenha) {
+		this.confirmarSenha = confirmarSenha;
 	}
 	
 }
